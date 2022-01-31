2769878d23d72e1b02ab4aad0f2b91061b4f6a5a
Mogakamo <mogaka.amo254@gmail.com>
2022-01-31 00:59:27 +0000

    5 is correct

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,31 +1,46 @@
 #!/usr/bin/python3
+"""import flask"""
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hellohbnb():
-	return "Hello HBNB!"
+    """returns hello HBNB"""
+    return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-	return "HBNB"
+    """returns HBNB"""
+    return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def Cisfun(text):
-	return 'C {:s}'.format(text.replace('_', ' '))
+    """returns C <custom_text>"""
+    return 'C {:s}'.format(text.replace('_', ' '))
+
 
 @app.route('/python/', defaults={'text': ' is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def Pythontxt(text):
-	return "Python {:s}".format(text.replace('_', ' '))
+    """returns Python <custom_text>"""
+    return "Python {:s}".format(text.replace('_', ' '))
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def num(n):
-	return '{} is a number'.format(n)
+    """ returns <n> is a number"""
+    return '{} is a number'.format(n)
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-	return render_template('5-number.html', n=n)
+    """ returns a template"""
+    return render_template('5-number.html', n=n)
+
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)

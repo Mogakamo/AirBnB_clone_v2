3d121706a0d0e2a6b3d4fb53051566643461e560
Mogakamo <mogaka.amo254@gmail.com>
2022-01-31 00:42:50 +0000

    1 is correct

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-"""
-flask model for route
-"""
+"""flask model for route"""
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hbnb():
-	"""
-		hbnb route page
-	"""
-	return 'Hello HBNB!'
+    """hnb route page"""
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def bnb():
-	"""
-		displays HBNB
-	"""
-	return 'HBNB'
+    """displays HBNB"""
+    return 'HBNB'
+
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
